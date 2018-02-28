d462837c732176a4e8b28a92fe869e832b7da623
Arthur Kiessling <Arthur Kiessling@10.91.16.75>
2018-02-28 10:20:20 +0100

    New Testclass

---
+++ src/at/spengergasse/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package at.spengergasse.test;
+import at.spengergasse.game.*;
+/**
+ * @author Arthur Kiessling
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Game.main(args);
+	}
+
+}
