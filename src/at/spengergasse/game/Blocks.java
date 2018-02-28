ff29ddbc63b409941d796b54a15d4a448a624fcb
Arthur Kiessling <Arthur Kiessling@DESKTOP-BI6P47L.home>
2018-02-28 22:22:10 +0100

    Weaponcheck updated

--- src/at/spengergasse/game/Blocks.java
+++ src/at/spengergasse/game/Blocks.java
@@ -7,14 +7,14 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 public class Blocks {
 	public static float speed=0.6f;
-	private static String BACKGROUND_IMAGE_LOC ="img/Background.png";
+	private static String BACKGROUND_IMAGE_LOC ="img/blocks/Background.png";
     private static Image backgroundImage;
     public static Node background;
     
-	private static final String BLOCK_IMAGE_LOC ="img/block.png";
+	private static final String BLOCK_IMAGE_LOC ="img/blocks/block.png";
 	public static int[] lifes;
 	
-	private static Node[] weapons;
+	static Node[] weapons;
 	
 	 public static Node newBlock(int x, int y) {
 	 Image blockImg= new Image(BLOCK_IMAGE_LOC);
@@ -63,11 +63,8 @@ public class Blocks {
 		}
 	 public static void heart(Group root) {
 		 lifes = new int[10];
-		 Node balken = new Rectangle(800,40,Color.BLACK);
-		 root.getChildren().add(balken);
-		 balken.relocate(0, 0);
 		 for(int i = 0; i< 5;i++) {
-			 Image heartimage = new Image("img/heart.png",32,32,false,false);
+			 Image heartimage = new Image("img/blocks/heart.png",32,32,false,false);
 			 Node heart = new ImageView(heartimage);
 			 lifes[i]=root.getChildren().size();
 			 root.getChildren().add(heart);
@@ -75,18 +72,18 @@ public class Blocks {
 			 
 		 }
 		 for(int i = 0; i< 5;i++) {
-			 Image heartimage = new Image("img/heart.png",32,32,false,false);
+			 Image heartimage = new Image("img/blocks/heart.png",32,32,false,false);
 			 Node heart = new ImageView(heartimage);
 			 lifes[5+i]=root.getChildren().size();
 			 root.getChildren().add(heart);
 			 heart.relocate(650+i*30, 5);
 		 }
 	}
-	 
+
 	 public static void weapons(Group root) {
 		 weapons =new Node[2];
 		 for(int i = 0; i<2;i++) {
-			 Image weaponsimage = new Image("img/weapon2.png");
+			 Image weaponsimage = new Image("img/weapons/weapon2.png");
 			 Node weapons = new ImageView(weaponsimage);
 			 root.getChildren().add(weapons);
 			 Blocks.weapons[i]=weapons;
@@ -95,14 +92,24 @@ public class Blocks {
 	}
 	 public static void weaponsDown() {
 		 double h = 0;
-	 for(int idx=0; idx<weapons.length;idx++){
-		 if(Physics.checkBoundsDown(weapons[idx])) { 
-			  h= weapons[idx].getBoundsInParent().getMinY()+speed;
-			}
-		if(!Physics.checkBoundsDown(weapons[idx])) { 
-			  h= weapons[idx].getBoundsInParent().getMinY()+4;
-			}
-		 weapons[idx].relocate(weapons[idx].getBoundsInParent().getMinX(),h);
-	 	}
+		 for(int i = 0; i<2;i++) {
+		 if(checkWeapons(weapons[i])==1||Game.player.weapon) {
+			 weapons[i].relocate(Game.player.player.getBoundsInParent().getMaxX()-20,Game.player.player.getBoundsInParent().getMinY()+10);
+		 }
+		 if(checkWeapons(weapons[i])==2||Game.player2.weapon) {
+			 weapons[i].relocate(Game.player2.player.getBoundsInParent().getMinX()-20,Game.player2.player.getBoundsInParent().getMinY()+10);
+		 }
 	 }
+	
+}
+	
+	  public static int checkWeapons(Node weapon) {
+				 if(weapon.getBoundsInParent().intersects(Game.player.player.getBoundsInParent().getWidth(), Game.player.player.getBoundsInParent().getHeight(),Game.player.player.getBoundsInParent().getMinX(),Game.player.player.getBoundsInParent().getMinY())) {
+					 return 1;
+				 }else
+				 if(weapon.getBoundsInParent().intersects(Game.player2.player.getBoundsInParent().getWidth(), Game.player2.player.getBoundsInParent().getHeight(),Game.player2.player.getBoundsInParent().getMinX(),Game.player2.player.getBoundsInParent().getMinY())) {
+					 return 2;
+				 }
+			return 3; 
+		}
 }
