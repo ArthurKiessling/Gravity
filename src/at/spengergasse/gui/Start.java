3224148d941443c0e7ea8ad7fd32db3018942492
Arthur Kiessling <Arthur Kiessling@DESKTOP-BI6P47L.home>
2018-06-04 21:46:57 +0200

    ,

--- src/at/spengergasse/gui/Start.java
+++ src/at/spengergasse/gui/Start.java
@@ -15,14 +15,24 @@ public class Start extends Application {
 	public static double W=800;
 	public static double H=800;
 
-	
+	/**
+	 *Application Start
+	 *Neues Control-Objekt wird erstellt
+	 *Alle Parameter werden an die Control-Klasse übergeben
+	 **/
 	@Override
 	public void start(Stage primaryStage){
 		new Controls(getParameters().getRaw());
 		
 	}
  
-
+	/**
+	 * Standart Konfiguration für den Background
+	 * @param primaryStage 
+	 * @param root
+	 * @param backgroundLink
+	 * @param iconLink
+	 **/
 	public static void genStartOptions(Stage primaryStage,Group root,InputStream backgroundLink,InputStream iconLink) {
 		Image icon=new Image(iconLink);
 		primaryStage.getIcons().addAll(icon);
